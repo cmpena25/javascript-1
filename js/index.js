760fe910cc4968c0d2bc4aebcd47c5612481a651
Celeste Pena <cmpena25@gmail.com>
2024-07-22 20:05:09 -0500

    created arithmetic functions

---
+++ js/index.js
@@ -0,0 +1,19 @@
+function addition(x,y){
+    console.log(x+y);
+}
+addition(2,2);
+addition(5,10);
+addition(6,3);
+addition(3,4);
+function subtraction(x,y){
+    console.log(x-y);
+}
+subtraction(10,2);
+function multiplication(x,y){
+    console.log(x*y);
+}
+multiplication(3,10);
+function division(x,y){
+    console.log(x/y);
+}
+division(20,2);
