837fb5029953d1a39491e09a98ff8c6c58e7cadd
Celeste Pena <cmpena25@gmail.com>
2024-07-24 22:19:36 -0500

    declared but did not initalize variable

--- js/introVariables.js
+++ js/introVariables.js
@@ -6,3 +6,14 @@ myName = "Michelle";
 console.log(myName);
 lastName = "Pena";
 console.log(lastName + ", " + myName);
+
+var character = 'hello';
+console.log(character);
+var quotations = "'hi'";
+console.log(quotations);
+
+let character = 'Hello';
+console.log(character);
+character = "World";
+let secondCharacter;
+console.log(secondCharacter);
